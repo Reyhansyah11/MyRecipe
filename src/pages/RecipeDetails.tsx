55292aa204fad082647d637255bce9aa8f2ee8b0
Reyhansyah11 <mr.reihansyah@gmail.com>
2024-10-27 14:13:36 +0700

    second commit

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import IngredientCard from "../components/IngredientCard";
 import { Link, useParams } from "react-router-dom";
 import { Recipe } from "../types/type";
 import axios from "axios";
